af6743ff1586e09688209e777fe9925cb145b954
Veres Zoltán <artidas@artidas.hu>
2023-12-21 11:50:04 +0100

    Did some fine tuning on the Fractal project.

--- Fractal_BASE_V1/javascript/sketch.js
+++ Fractal_BASE_V1/javascript/sketch.js
@@ -1,8 +1,10 @@
 let canvas = new Canvas();
 
 // Project variables
-let trianglePoints = [];
-let angle_slider, global_angle_slider, scaling_factor_slider;
+let slider_space_height = 150;
+let angle_slider, global_angle_slider, scaling_factor_slider, 
+    minimum_length_slider, branches_slider
+;
 
 /** ********************************************************************
  ** *** MAIN ENTRY FUNCTION ********************************************
@@ -12,55 +14,69 @@ function setup() {
 
     canvas.object = createCanvas(
         canvas.getWidth(),
-        canvas.getHeight() - 120
+        canvas.getHeight() - slider_space_height
     );
 
-    angle_slider = createSlider(0, PI, PI / 4, 0.01); // Slider for adjusting angle
+    angle_slider = createSlider(0, PI, PI / 3, PI / 1000); // Slider for adjusting angle
     angle_slider.size(canvas.getWidth() - 20 - 200);
-    angle_slider.position(200, canvas.getHeight() - 120 + 20);
-    createP("Angle:").position(20, canvas.getHeight() - 120 - 15);
+    angle_slider.position(200, canvas.getHeight() - slider_space_height + 20);
+    createP("Angle:").position(20, canvas.getHeight() - slider_space_height);
 
-    global_angle_slider = createSlider(0, PI, 0, 0.01); // Slider for adjusting global angle
+    global_angle_slider = createSlider(0, PI, PI / 2, PI / 1000); // Slider for adjusting global angle
     global_angle_slider.size(canvas.getWidth() - 20 - 200);
-    global_angle_slider.position(200, canvas.getHeight() - 120 + 50);
-    createP("Global Angle:").position(20, canvas.getHeight() - 120 + 15);
+    global_angle_slider.position(200, canvas.getHeight() - slider_space_height + 50);
+    createP("Global Angle:").position(20, canvas.getHeight() - slider_space_height + 30);
 
-    scaling_factor_slider = createSlider(0, 0.8, 0.67, 0.01); // Slider for adjusting scaling factor
+    scaling_factor_slider = createSlider(0, 0.8, 0.5, 0.01); // Slider for adjusting scaling factor
     scaling_factor_slider.size(canvas.getWidth() - 20 - 200);
-    scaling_factor_slider.position(200, canvas.getHeight() - 120 + 80);
-    createP("Scaling Factor:").position(20, canvas.getHeight() - 120 + 45);
-    
-    //noLoop();
-};
+    scaling_factor_slider.position(200, canvas.getHeight() - slider_space_height + 80);
+    createP("Scaling Factor:").position(20, canvas.getHeight() - slider_space_height + 60);
+
+    minimum_length_slider = createSlider(1, 10, 3, 0.1); // Slider for adjusting scaling factor
+    minimum_length_slider.size(canvas.getWidth() - 20 - 200);
+    minimum_length_slider.position(200, canvas.getHeight() - slider_space_height + 110);
+    createP("Minimum length:").position(20, canvas.getHeight() - slider_space_height + 90);
+
+    branches_slider = createSlider(1, 5, 3, 1); // Slider for adjusting the number of branches
+    branches_slider.size(canvas.getWidth() - 20 - 200);
+    branches_slider.position(200, canvas.getHeight() - slider_space_height + 140);
+    createP("Number of Branches:").position(20, canvas.getHeight() - slider_space_height + 120);
+
+    // noLoop();
+}
 
 /** ********************************************************************
  ** *** MAIN LOOPING FUNCTION ******************************************
  ** ********************************************************************/
 function draw() {
-    console.log('Next cycle...');
     background('#ddd');
 
-    stroke(0);
+    strokeWeight(2);
+
     translate(canvas.getWidth() / 2, canvas.getHeight() - 120);
-    branch((canvas.getHeight() - 120) / 3);
+    branch((canvas.getHeight() - 120) / 3, branches_slider.value());
 
-    //noLoop();
-};
+    // noLoop();
+}
 
-function branch(len) {
+function branch(length, branches) {
     // Recursive function to draw branches of the fractal tree
-    line(0, 0, 0, -len);
-    translate(0, -len);
+    line(0, 0, 0, -length);
+    translate(0, -length);
 
-    if (len > 4) {
-        push();
-        rotate(angle_slider.value() + global_angle_slider.value()); // Rotate to the right
-        branch(len * scaling_factor_slider.value()); // Recursive call
-        pop();
-
-        push();
-        rotate(-angle_slider.value() + global_angle_slider.value()); // Rotate to the left
-        branch(len * scaling_factor_slider.value()); // Recursive call
-        pop();
+    if (length > minimum_length_slider.value()) {
+        for (let i = 0; i < branches; i++) {
+            push();
+            rotate(
+                global_angle_slider.value() + 
+                map(
+                    i,
+                    0, branches - 1,
+                    -PI / 2, PI / 2 + angle_slider.value()
+                )
+            );
+            branch(length * scaling_factor_slider.value(), branches_slider.value()); // Recursive call
+            pop();
+        }
     }
 }
--- Fractal_BASE_V2/javascript/sketch.js
+++ Fractal_BASE_V2/javascript/sketch.js
@@ -2,71 +2,54 @@ let canvas = new Canvas();
 
 // Project variables
 let trianglePoints = [];
-let a = 1; // Coefficient of x^2
-let b = 2; // Coefficient of x
-let c = 1; // Constant
-
-let a_slider, b_slider, c_slider;
+let slider_space_height = 50;
+let margin = 20;
+let depth_slider;
 
 /** ********************************************************************
  ** *** MAIN ENTRY FUNCTION ********************************************
  ** ********************************************************************/
 function setup() {
+    frameRate(10);
+
     canvas.object = createCanvas(
         canvas.getWidth(),
-        canvas.getHeight() - 120
-    );
-    
-    a_slider = createSlider(-1.1, 2, a, 0.0001);
-    b_slider = createSlider(
-        -canvas.getWidth() / 10,
-        canvas.getWidth() / 10,
-        b,
-        0.0001
-    );
-    c_slider = createSlider(
-        -canvas.getHeight() / 2,
-        canvas.getHeight() / 2,
-        c,
-        0.1
+        canvas.getHeight() - slider_space_height
     );
 
-    a_slider.position(20, canvas.getHeight() - 120 + 20);
-    b_slider.position(20, canvas.getHeight() - 120 + 50);
-    c_slider.position(20, canvas.getHeight() - 120 + 80);
-
-    a_slider.input(updateValues);
-    b_slider.input(updateValues);
-    c_slider.input(updateValues);
+    depth_slider = createSlider(0, 10, 0, 1); // Slider for adjusting depth
+    depth_slider.size(canvas.getWidth() - 20 - 200);
+    depth_slider.position(200, canvas.getHeight() - slider_space_height + 20);
+    createP("Depth:").position(20, canvas.getHeight() - slider_space_height);
 
     // Initial vertices of an equilateral triangle
-    let p1 = createVector(canvas.getWidth() / 2, (canvas.getHeight() - 120) - 50);
-    let p2 = createVector(canvas.getWidth() / 4, (canvas.getHeight() - 120) / 4);
-    let p3 = createVector((3 * canvas.getWidth()) / 4, (canvas.getHeight() - 120) / 4);
+    let p1 = createVector(canvas.getWidth() / 2, margin);
+    let p2 = createVector(margin, (canvas.getHeight() - slider_space_height) - margin);
+    let p3 = createVector(canvas.getWidth() - margin, (canvas.getHeight() - slider_space_height) - margin);
     
     trianglePoints = [p1, p2, p3];
-    
-    //noLoop();
-};
+
+    // noLoop();
+}
 
 /** ********************************************************************
  ** *** MAIN LOOPING FUNCTION ******************************************
  ** ********************************************************************/
 function draw() {
-    console.log('Next cycle...');
-    frameRate(3);
     background('#ddd');
 
-    stroke(0);
-    fill(0, 0, 0);
-    drawTriangle(trianglePoints, 5);
+    fill(0);
+    strokeWeight(0);
 
-    //noLoop();
-};
+    drawTriangle(trianglePoints, depth_slider.value());
 
-//Sierpinski Triangle
+    // noLoop();
+}
+
+// Sierpinski Triangle
 function drawTriangle(points, depth) {
     if (depth === 0) {
+        // Draw the triangle with vertices flipped horizontally
         triangle(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y);
     } else {
         // Calculate midpoints of the sides
@@ -79,11 +62,4 @@ function drawTriangle(points, depth) {
         drawTriangle([mid1, points[1], mid2], depth - 1);
         drawTriangle([mid3, mid2, points[2]], depth - 1);
     }
-}
-
-function updateValues() {
-    // Update the values of a, b, and c from the sliders
-    a = a_slider.value();
-    b = b_slider.value();
-    c = c_slider.value();
 }
