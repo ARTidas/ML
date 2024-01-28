89240ba2b6f6368810cbc7bc3f19d5bb44be04d2
Veres Zoltán <artidas@artidas.hu>
2024-01-28 12:43:33 +0100

    Created a scale for the coordinate system.

--- DrawingBoard_BASE_V2/javascript/sketch.js
+++ DrawingBoard_BASE_V2/javascript/sketch.js
@@ -1,22 +1,30 @@
 let canvas = new Canvas();
 
-let slider_space_height = 40 * 2;
+let slider_space_height = 40 * 1;
 let stroke_weight_slider;
+
+const origin_x = canvas.getWidth() / 2;
+const origin_y = (canvas.getHeight() - slider_space_height) / 2;
 
 /** ********************************************************************
  ** *** MAIN ENTRY FUNCTION ********************************************
  ** ********************************************************************/
 function setup() {
-    let shortest_canvas_side= min(
+    /*let shortest_canvas_side= min(
         canvas.getWidth(),
         canvas.getHeight() - slider_space_height
     )
     canvas.object = createCanvas(
         shortest_canvas_side,
         shortest_canvas_side
+    );*/
+    canvas.object = createCanvas(
+        canvas.getWidth(),
+        canvas.getHeight() - slider_space_height
     );
 
-    stroke_weight_slider = createSlider(0, 80, 30, 1); // Slider for adjusting angle
+    //stroke_weight_slider = createSlider(0, 120, 60, 1); // Slider for adjusting the stroke width
+    stroke_weight_slider = createSlider(0, 120, 30, 1);
     stroke_weight_slider.size(canvas.getWidth() - 20 - 200);
     stroke_weight_slider.position(200, canvas.getHeight() - slider_space_height + 20);
     createP("Stroke weight:").position(20, canvas.getHeight() - slider_space_height);
@@ -39,6 +47,7 @@ function draw() {
     console.log('Next cycle...');
     //frameRate(20);
 
+    drawCoordinateSystem();
     strokeWeight(stroke_weight_slider.value());
 
     if (mouseIsPressed) {
@@ -46,6 +55,76 @@ function draw() {
     }
 
     //noLoop();
+}
+
+
+function drawCoordinateSystem() {
+    //noStroke();
+    strokeWeight(1);
+
+    line(origin_x, 0, origin_x, canvas.getHeight());
+    drawArrow(canvas.getWidth(), origin_y, 10, 3, 'right');
+    drawScales(10);
+    
+    line(0, origin_y, canvas.getWidth(), origin_y);
+    drawArrow(origin_x, 0, 10, 3, 'up');
+}
+
+function drawArrow(x, y, length, size, direction = null) {
+    switch (direction) {
+        case 'up':
+            direction = HALF_PI;
+            break;
+        case 'down':
+            direction = -HALF_PI;
+            break;
+        case 'left':
+            direction = -PI;
+            break;
+        case 'right':
+            direction = PI;
+            break;
+        default:
+            direction = 0;
+    }
+
+    push(); // push-pop pair will help to elimintate resetting the translate function
+    translate(x, y);
+    rotate(direction);
+    line(0, 0, length - size, -size);
+    line(0, 0, length - size, size);
+    pop();
+}
+
+function drawScales(scale) {
+    for (let i = 0; i < ((canvas.getWidth() / scale) / 2); i++) {
+        line(
+            origin_x + i * scale,
+            origin_y - scale / 2,
+            origin_x + i * scale,
+            origin_y + scale / 2
+        );
+        line(
+            origin_x - i * scale,
+            origin_y - scale / 2,
+            origin_x - i * scale,
+            origin_y + scale / 2
+        );
+    }
+    for (let i = 0; i < (((canvas.getHeight() - slider_space_height) / scale) / 2); i++) {
+        line(
+            origin_x + scale / 2,
+            origin_y + i * scale,
+            origin_x - scale / 2,
+            origin_y + i * scale
+        );
+        line(
+            origin_x + scale / 2,
+            origin_y - i * scale,
+            origin_x - scale / 2,
+            origin_y - i * scale
+        );
+    }
 }
 
 
@@ -62,5 +141,12 @@ function clearCanvas() {
 }
 
 function saveDrawing() {
-    saveCanvas(canvas.object, 'canvas_drawing_' + Date.now(), 'png');
+    //saveCanvas(canvas.object, 'canvas_drawing_' + Date.now(), 'png');
+
+    // Create a new off-screen canvas with the desired size (28x28 pixels)
+    let resizedCanvas = createGraphics(28, 28);
+    // Draw the original canvas onto the new off-screen canvas, rescaled
+    resizedCanvas.image(canvas.object, 0, 0, resizedCanvas.width, resizedCanvas.height);
+    // Save the resized canvas
+    saveCanvas(resizedCanvas, 'canvas_drawing_resized_' + Date.now(), 'png');
 }
