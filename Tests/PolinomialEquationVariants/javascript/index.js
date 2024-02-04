4e14aaa6c4c4c8b9510f4004a521b922180de15f
Veres Zoltán <artidas@artidas.hu>
2024-02-04 14:41:18 +0100

    Modified the placement of the .innerHTML function as constantly calling inside a loop caused performance issues.

--- Tests/PolinomialEquationVariants/javascript/index.js
+++ Tests/PolinomialEquationVariants/javascript/index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-    const output_container = document.getElementById('output');
-    let counter = 0;
-
     run();
 
     // Cartesian products of several sets
     // https://en.wikipedia.org/wiki/Cartesian_product
     function run() {
+        const output_container = document.getElementById('output');
+        let counter = 0;
+        let content = '';
+
         //+- a x^n +- b x +- c
         const signum_values = ['', '+', '-'];
         const operation_values = ['+', '-']; // TODO: Add multiplication * and division /
@@ -16,46 +16,55 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const b_values = ['0', '1', '1.1', '2', '3', '7.89', '78.9', '789'];
         const c_values = ['0', '1', '1.1', '2', '3', '2.46', '24.6', '246'];
 
-        /*output_container.innerHTML += '//---ax^n--------------------------------------------------------------------------<br/>'
+
+        output_container.innerHTML += '//---ax^n--------------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < a_values.length; j++) {
                 for (let k = 0; k < n_values.length; k++) {
-                    output_container.innerHTML += (
+                    content += (
                         (++counter + '.: ') + 
                         (signum_values[i] + a_values[j] + 'x^{' + n_values[k] + '}' + "<br/>")
                     );
                 }
             }
         }
+        output_container.innerHTML += content;
 
-        output_container.innerHTML += '//---bx---------------------------------------------------------------------------<br/>'
+
+        output_container.innerHTML += '//---bx---------------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < b_values.length; j++) {
-                output_container.innerHTML += (
+                content += (
                     (++counter + '.: ') + 
                     (signum_values[i] + b_values[j] + 'x' + "<br/>")
                 );
             }
         }
+        output_container.innerHTML += content;
 
-        output_container.innerHTML += '//---c----------------------------------------------------------------------------<br/>'
+        output_container.innerHTML += '//---c----------------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < c_values.length; j++) {
-                output_container.innerHTML += (
+                content += (
                     (++counter + '.: ') + 
                     (signum_values[i] + c_values[j] + "<br/>")
                 );
             }
         }
+        output_container.innerHTML += content;
 
-        output_container.innerHTML += '//---ax^n+-bx---------------------------------------------------------------------<br/>'
+        output_container.innerHTML += '//---ax^n+-bx---------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < a_values.length; j++) {
                 for (let k = 0; k < n_values.length; k++) {
                     for (let l = 0; l < operation_values.length; l++) {
                         for (let m = 0; m < signum_values.length; m++) {
                             for (let n = 0; n < b_values.length; n++) {
-                                output_container.innerHTML += (
+                                content += (
                                     (++counter + '.: ') + 
                                     (signum_values[i] + a_values[j] + 'x^{' + n_values[k] + '}') +
                                     (operation_values[l]) +
@@ -67,15 +76,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             }
         }
+        output_container.innerHTML += content;
 
-        output_container.innerHTML += '//---ax^n+-c----------------------------------------------------------------------<br/>'
+        output_container.innerHTML += '//---ax^n+-c----------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < a_values.length; j++) {
                 for (let k = 0; k < n_values.length; k++) {
                     for (let l = 0; l < operation_values.length; l++) {
                         for (let m = 0; m < signum_values.length; m++) {
                             for (let n = 0; n < c_values.length; n++) {
-                                output_container.innerHTML += (
+                                content += (
                                     (++counter + '.: ') + 
                                     (signum_values[i] + a_values[j] + 'x^{' + n_values[k] + '}') +
                                     (operation_values[l]) +
@@ -87,14 +98,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             }
         }
+        output_container.innerHTML += content;
 
-        output_container.innerHTML += '//---bx+-c------------------------------------------------------------------------<br/>'
+        output_container.innerHTML += '//---bx+-c------------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < b_values.length; j++) {
                 for (let k = 0; k < operation_values.length; k++) {
                     for (let l = 0; l < signum_values.length; l++) {
                         for (let m = 0; m < c_values.length; m++) {
-                            output_container.innerHTML += (
+                            content += (
                                 (++counter + '.: ') + 
                                 (signum_values[i] + b_values[j] + 'x') + 
                                 (operation_values[k]) +
@@ -104,9 +117,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     }
                 }
             }
-        }*/
+        }
+        output_container.innerHTML += content;
 
-        /*output_container.innerHTML += '//---ax^n+-bx+-c------------------------------------------------------------------<br/>'
+        output_container.innerHTML += '//---ax^n+-bx+-c------------------------------------------------------------------<br/>';
+        content = '';
         for (let i = 0; i < signum_values.length; i++) {
             for (let j = 0; j < a_values.length; j++) {
                 for (let k = 0; k < n_values.length; k++) {
@@ -116,7 +131,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                                 for (let o = 0; o < operation_values.length; o++) {
                                     for (let p = 0; p < signum_values.length; p++) {
                                         for (let q = 0; q < c_values.length; q++) {
-                                            output_container.innerHTML += (
+                                            content += (
                                                 (++counter + '.: ') + 
                                                 (signum_values[i] + a_values[j] + 'x^{' + n_values[k] + '}') +
                                                 (operation_values[l]) +
@@ -133,7 +148,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     }
                 }
             }
-        }*/
+        }
+        output_container.innerHTML += content;
         
     }
 
