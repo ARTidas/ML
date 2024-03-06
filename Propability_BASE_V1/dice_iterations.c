09fb353240263389b176730093e4288b5ec49203
Veres Zoltán <artidas@artidas.hu>
2024-03-06 23:36:29 +0100

    Extended dice iterations script.

--- Propability_BASE_V1/dice_iterations.c
+++ Propability_BASE_V1/dice_iterations.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+int counter;
+
 int main()
 {
-    int counter = 0;
-    
+    counter = 0;
+    printf("Both dice throws add up to at least 8:\n");
     for (int i = 1; i < 7; i++) {
         for (int j = 1; j < 7; j++) {
             if (i + j >= 8) {
@@ -12,8 +14,21 @@ int main()
             }
         }
     }
-    
-    printf("Total possibilities: %d", counter);
+    printf("\nTotal possibilities: %d\n", counter);
+    printf("====================================\n");
+
+    counter = 0;
+    printf("First dice throw is greater than the second:\n");
+    for (int i = 1; i <= 6; i++) {
+        for (int j = 1; j <= 6; j++) {
+            if (i > j) {
+                counter++;
+                printf("(%d,%d);", i, j);
+            }
+        }
+    }
+    printf("\nToal possibilities: %d\n", counter);
+    printf("====================================\n");
 
     return 0;
 }
